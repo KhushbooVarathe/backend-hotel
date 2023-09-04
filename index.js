7e7f2b5a00554ca4bf00437e56d3ad39453e2d6b
KhushbooVarathe <khushboo.va@cisinlabs.com>
2023-09-04 18:39:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express')
+const app = express()
+require('dotenv').config();
+const auth=require('./routes/auth')
+const hotels=require('./routes/hotels')
+const rooms=require('./routes/rooms')
+const users=require('./routes/users')
+const path=require('path')
+var cors = require('cors')
+app.use(express.json())
+app.use(cors())
+
+// const HotelSchema=require('./modals/Hotels')
+var cookieParser = require('cookie-parser')
+// console.log("hello world")
+require('./modals/config')
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname,'public')));
+app.get('*',(async(req,res)=>{
+    res.send(path.join(__dirname,'public'))
+}))
+// Serve the static files from the public folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+app.use('/api',hotels)
+
+app.use('/api',users)
+
+app.use((err,req,res,next)=>{
+    // console.log("i am middleware",err,"errrrrrrrrrr",req.body)
+    const errorStatus=err.status || 500
+    const errMessage=err.message||"something went wrong"
+    next();
+    // return res.send("okkkkk")
+//    return res.status(errorStatus).json({success:false,
+// status:errorStatus,
+// message:errMessage,
+// stack:err.stack
+// })
+
+})
+app.use('/api',rooms)
+app.use('/api',auth)
+app.listen(7000,()=>{
+    console.log("runnig at port 7000")
+})
